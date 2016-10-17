576e4b8d7ffdf42f439a5d4aeee95f6f6a126e38
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-10-17 22:24:26 +0300

    checking the best way for matching

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
-function fileType (chunk)
+function fileType (buffer)
   local buf = {}
 
-  for i = 1, tonumber(chunk.length) do
-    buf[i] = string.upper(bit.tohex(chunk[i], 2))
+  for i = 1, tonumber(buffer.length) do
+    buf[i] = string.upper(bit.tohex(buffer[i], 2))
   end
 
-  if buf[1] == '89' and buf[2] == '50' and buf[3] === '4E' and buf[4] == '47' then
+  if buf[1] == '89' and buf[2] == '50' and buf[3] == '4E' and buf[4] == '47' then
     return {ext = 'png', mime = 'image/png'}
   end
 
